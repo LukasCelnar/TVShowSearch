caad75c95e91e1bbceffcf17a3e49a78fa7bb489
Drizz159 <celnar.lukas@gmail.com>
2020-08-03 02:13:18 +0200

    Updates

---
+++ precache-manifest.738365e0574797f1c9be9cd13440be64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f0f26947468c6206c7095d9d809a42",
+    "url": "/TVShowSearch/index.html"
+  },
+  {
+    "revision": "acd5062d12db1e04765f",
+    "url": "/TVShowSearch/static/css/main.06baeb85.chunk.css"
+  },
+  {
+    "revision": "9ec4762cb468c0b9d12a",
+    "url": "/TVShowSearch/static/js/2.fc8ab9ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TVShowSearch/static/js/2.fc8ab9ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd5062d12db1e04765f",
+    "url": "/TVShowSearch/static/js/main.70301067.chunk.js"
+  },
+  {
+    "revision": "ee3b7abe570190e93052",
+    "url": "/TVShowSearch/static/js/runtime-main.9cfb68eb.js"
+  }
+]);
